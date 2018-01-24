a5a7ce570236b9bfb158cff78740f1e89777bf5b
cbuelvasc <carmelo.buelvas@globant.com>
2018-01-24 10:36:16 -0500

    Ajustes profile

--- src/test/java/com/globant/screen/ios/HomeScreenIOS.java
+++ src/test/java/com/globant/screen/ios/HomeScreenIOS.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component("homeScreenIOS")
+@Component
 @Profile("IOS")
 public class HomeScreenIOS extends BaseScreen {
 
--- src/test/java/com/globant/test/AddContactsAndroid.java
+++ src/test/java/com/globant/test/AddContactsAndroid.java
@@ -3,8 +3,6 @@ package com.globant.test;
 import com.globant.screen.android.HomeScreenAndroid;
 import com.globant.screen.android.NewContactScreenAndroid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 import org.testng.annotations.Test;
 
@@ -15,8 +13,7 @@ import static com.globant.util.MapperJSON.getUser;
 @ActiveProfiles("ANDROID")
 public class AddContactsAndroid extends BaseTests {
 
-    @Autowired(required = true)
-    @Qualifier("homeScreenAndroid")
+    @Autowired
     private HomeScreenAndroid homeScreenAndroid;
     private NewContactScreenAndroid newContactScreenAndroid;
 
